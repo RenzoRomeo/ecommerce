1ad6d902f94f8c5f7a1020f8e5c7b37a08b38eb4
Renzo Romeo <renzoromeo44@gmail.com>
2022-02-06 20:05:52 -0300

    searchbar

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,45 +1,25 @@
-import { Stack, Input, IconButton } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
-import React, { useState } from 'react';
+import { Stack, Input } from '@chakra-ui/react';
+import { useState } from 'react';
 import { getProductsResult, ProductType } from '../products';
 import SearchbarItem from './searchbar-item';
 
 const Searchbar = () => {
-  const [query, setQuery] = useState<string>('');
   const [results, setResults] = useState<Array<ProductType>>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-    setResults(getProductsResult(query));
-  };
-
-  const handleClick = (
-    e:
-      | React.MouseEvent<HTMLButtonElement>
-      | React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    e.preventDefault();
-    setResults(getProductsResult(query));
+    setResults(getProductsResult(e.target.value));
   };
 
   return (
     <Stack direction="column">
-      <Stack direction="row" w="20vw">
-        <Input
-          color="white"
-          borderWidth={2}
-          borderColor="whiteAlpha.600"
-          placeholder="Search an item"
-          onChange={handleChange}
-          onKeyPress={(e) => e.key === 'Enter' && handleClick(e)}
-        />
-        <IconButton
-          aria-label="search"
-          icon={<SearchIcon />}
-          display="inline-block"
-          onClick={handleClick}
-        />
-      </Stack>
+      <Input
+        color="white"
+        w="20vw"
+        borderWidth={2}
+        borderColor="whiteAlpha.600"
+        placeholder="Search an item"
+        onChange={handleChange}
+      />
 
       <Stack
         direction="column"
