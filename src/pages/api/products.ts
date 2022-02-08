3b2b187db985f63af1672918f6fa490a2fa30f69
Renzo Romeo <renzoromeo44@gmail.com>
2022-02-08 11:14:50 -0300

    products

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,7 +1,7 @@
-import products from "../../util/products";
+import { getAllProducts } from '../../util/products';
 
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function Handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(products);
+  res.status(200).json(getAllProducts());
 }
